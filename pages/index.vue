720c1433077cc6284239b640a485180520f1e201
Adedamola Elijah <tmp10659@firstbanknigeria.com>
2021-03-16 17:47:20 +0100

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,7 @@
         My astonishing Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+       <h3>hello</h3>
       </div>
     </div>
   </div>
