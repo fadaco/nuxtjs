ed9928a32af50502d462c853fe433f73d2335ca5
Adedamola Elijah <tmp10659@firstbanknigeria.com>
2021-03-18 16:00:11 +0100

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,39 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <v-app class="app">
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+<style scoped>
+.app {
+  font-family: "Nunito Sans", sans-serif;
 }
 </style>
+
+<script>
+export default {
+  head: {
+    script: [
+      {
+        src="jquery.min.js",
+        type: "text/javascript"
+      }
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap"
+      },
+      {
+        rel: 'stylesheet',
+        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
+      },
+      {
+        rel: 'stylesheet',
+        href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css"
+      },
+    ]
+  }
+}
